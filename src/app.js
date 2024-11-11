5e1990b0e5238a61839aa3d08ea7615e5c497087
jinal <jilu.jr11@gmail.com>
2024-11-12 00:16:17 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,15 +2,30 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+    // res.send("hey you want to get me? nobody gets me i am the buzz lightyear")
+    res.send({name: "jinal", lastname: "rathva"})
+});
+
+app.post("/user", (req, res) => {
+    console.log("save data to the database");
+    res.send("data successfully saved to the database");
+});
+
+app.delete("/user", (req, res) => {
+    console.log("get lost");
+    res.send("data successfully deleted from the database");
+});
+
+
 
 app.use( "/test",(req, res) => {
-    res.send("hello from the server side")
+    res.send("hello from the server side - andy is speaking")
 });
-app.use( "/hello",(req, res) => {
-    res.send("hello ")
-});
+app.u
+
 
 
 app.listen(3000, () => {
-    console.log("chalu hogaya bc")
+    console.log("buzz lightyear speaking on server")
 });
