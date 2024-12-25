64ea9286db66efb74d1ccf0fba2aba8b0cb31a5d
jinal <jilu.jr11@gmail.com>
2024-12-25 21:05:36 +0530

    backend update

--- src/routes/user.js
+++ src/routes/user.js
@@ -85,7 +85,7 @@ userRouter.get("/feed", userAuth, async(req,res)=> {
        const page = parseInt(req.query.page) || 1;
        let limit = parseInt(req.query.limit) || 10;
         limit = limit>50 ? 50 : limit;
-       const skip = (page-1)*limit
+        const skip = (page-1)*limit
        
 
 
